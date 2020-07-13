8d79c71412057e7bd14c73932e3f6e89d6d4c239
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-07-13 17:57:08 -0300

    faster version with translate

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -2,6 +2,9 @@ from collections import Counter
 from string import punctuation
 
 def count_words(sentence):
-    words = sentence.replace(',', ' ').replace('_', ' ').lower().split()
-    words = [word.strip(punctuation) for word in words]
+    # string.punctuation without single '
+    trans_regex = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
+    trans_engine = str.maketrans(trans_regex, ' '*len(trans_regex))
+    words = str.translate(sentence, trans_engine).lower().split()
+    words = [word.strip('\'') for word in words]
     return dict(Counter(words))
