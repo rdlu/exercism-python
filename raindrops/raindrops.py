c248261ce3adc957afefe309df345357842a03bc
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-07-13 16:47:22 -0300

    raindrops exercise

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,8 @@
 def convert(number):
-    pass
+    queue = []
+    if number % 3 == 0: queue.append('Pling')
+    if number % 5 == 0: queue.append('Plang')
+    if number % 7 == 0: queue.append('Plong')
+    if not queue: queue.append(str(number))
+
+    return ''.join(queue)
