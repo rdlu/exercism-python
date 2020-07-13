8c1b08bafa3672736520fc2aaca029b2efc6e700
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-07-13 17:59:41 -0300

    comments

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -4,6 +4,7 @@ from string import punctuation
 def count_words(sentence):
     # string.punctuation without single '
     trans_regex = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
+    # compiling a translating engine for faster replace
     trans_engine = str.maketrans(trans_regex, ' '*len(trans_regex))
     words = str.translate(sentence, trans_engine).lower().split()
     words = [word.strip('\'') for word in words]
