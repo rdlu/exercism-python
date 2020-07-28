7edbaafc5cb202ffcc76b42be61cc68de74c263a
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-07-28 16:59:44 -0300

    etl v2, more pythonic?

--- etl/etl.py
+++ etl/etl.py
@@ -1,6 +1,4 @@
 def transform(legacy_data):
-    new_data = {}
-    for point in legacy_data:
-        for value in legacy_data[point]:
-            new_data[value.lower()] = point
-    return new_data
+    return { letter.lower(): score 
+        for score, letters in legacy_data.items() 
+        for letter in letters }
