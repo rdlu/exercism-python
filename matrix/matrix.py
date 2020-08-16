bd69f0bfe5d13a0e60bfa144b397c81383c03d2e
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-16 18:57:35 -0300

    second matrix version

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,15 +1,17 @@
-class Matrix:
+class Matrix(object):
     def __init__(self, matrix_string):
-        list_of_rows_str = matrix_string.split('\n')
-        self.list_of_rows = []
-        for row in list_of_rows_str:
-            self.list_of_rows.append([int(value) for value in row.split(' ')])
+        self._matrix = [list(map(int, r.split())) for r in matrix_string.split('\n')]
+
+    @property
+    def rows(self):
+        return self._matrix[:]
+
+    @property
+    def columns(self):
+        return list(map(list, zip(*self._matrix)))
 
     def row(self, index):
-        return self.list_of_rows[index-1]
+        return self.rows[index-1]
 
     def column(self, index):
-        col = []
-        for row in self.list_of_rows:
-            col.append(row[index-1])
-        return col
+        return self.columns[index-1]
