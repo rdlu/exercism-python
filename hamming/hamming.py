681923a12239f1c596cdf436d2bf33aae8844aa4
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-16 20:04:24 -0300

    hamming, pythonic, comparison before

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Different lengths on strands')
-    return sum(i != j for i, j in zip(strand_a, strand_b))
+    return sum(1 for i, j in zip(strand_a, strand_b) if i != j)
