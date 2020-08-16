6614662bfc78cc20ecc961e365fd858c27784a1e
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-16 19:58:17 -0300

    hamming using zip, passing all tests

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,6 +1,8 @@
 def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Different lengths on strands')
     counter = 0
-    for idx, val in enumerate(strand_a):
-        if val != strand_b[idx]:
-            counter += 1
+    for l, r in zip(strand_a, strand_b):
+        if l != r:
+          counter += 1
     return counter
