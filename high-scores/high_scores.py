f46192720e7ac97a9563429272021240b99c9eba
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-16 19:10:58 -0300

    formatting

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,12 +1,8 @@
-from itertools import islice
-
 def latest(scores):
     return scores[-1]
 
-
 def personal_best(scores):
     return max(scores)
-
 
 def personal_top_three(scores):
     return sorted(scores, reverse=True)[:3]
