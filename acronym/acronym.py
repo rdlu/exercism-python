2b3b8f3e636bb1a7bd8e162373927fa9fd0b250f
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-17 11:23:29 -0300

    acronym, simple version

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,8 +1,5 @@
+import re
+
 def abbreviate(sentence):
-    # string.punctuation without single '
-    trans_regex = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
-    # compiling a translating engine for faster replace
-    trans_engine = str.maketrans(trans_regex, ' '*len(trans_regex))
-    words = str.translate(sentence, trans_engine).lower().split()
-    words = [word.strip('\'') for word in words]
+    words = re.findall(r"[a-zA-Z0-9]+\'{0,1}[a-zA-Z0-9]*", sentence)
     return ''.join([word[0].upper() for word in words])
