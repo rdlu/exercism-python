1d9c4ae06f17902cb4e6c2558ad5604ca5baf480
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-17 12:52:26 -0300

    kindergarten 2

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -9,19 +9,14 @@ class Garden:
         'R': "Radishes", 'G': "Grass", 'C': "Clover", 'V': "Violets"
     }
 
-    def __init__(self, diagram: str, students: list = None):
+    def __init__(self, diagram: str, students: list = _students):
         self.diagram = diagram.split()
-        if students:
-            self._students = sorted(students)
+        self._students = sorted(students)
 
     def plants(self, student: str) -> list:
         initials = self._get_initials(student)
         return [self._plants_table[initial] for rows in initials for initial in rows]
 
-    def _calculate_position(self, student: str) -> int:
-        idx = self._students.index(student)
-        return idx * 2
-
     def _get_initials(self, student: str) -> list:
-        col = self._calculate_position(student)
-        return [row[col:col+2] for row in self.diagram]
+        idx = self._students.index(student) * 2
+        return [row[idx:idx+2] for row in self.diagram]
