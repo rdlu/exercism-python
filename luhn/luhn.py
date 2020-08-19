5b8bf1a3fdcb2e045250413b7228bdf3b455337c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-19 16:34:37 -0300

    oops

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -5,10 +5,10 @@ class Luhn:
         self._odd_digits = self._reversed_card_num[::2]
 
     def valid(self) -> bool:
-        if str.isnumeric(self._reversed_card_num) and len(self._reversed_card_num) < 2:
+        if str.isnumeric(self._reversed_card_num) and len(self._reversed_card_num) > 1:
             return self._sum_card() % 10 == 0
-        else:
-            return False 
+        else: 
+            return False
         
     def _sum_card(self) -> int:
         card_sum = 0
