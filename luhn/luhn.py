c03fb52226f9e2f177c8bf84bb871bc6061e4f17
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-19 16:32:37 -0300

    luhn: readiblity on if else

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -5,9 +5,10 @@ class Luhn:
         self._odd_digits = self._reversed_card_num[::2]
 
     def valid(self) -> bool:
-        if not str.isnumeric(self._reversed_card_num) or len(self._reversed_card_num) < 2:
-            return False
-        return self._sum_card() % 10 == 0
+        if str.isnumeric(self._reversed_card_num) and len(self._reversed_card_num) < 2:
+            return self._sum_card() % 10 == 0
+        else:
+            return False 
         
     def _sum_card(self) -> int:
         card_sum = 0
