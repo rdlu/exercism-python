5c0634f3439004fdca7f0a0cdaf5c58ea0cccac1
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-24 11:50:42 -0300

    simpler clock: tracking only minutes and convert

--- clock/clock.py
+++ clock/clock.py
@@ -1,25 +1,17 @@
 class Clock:
     def __init__(self, hour, minutes):
-        self._hour, self._minutes = self.convert_to_clock(hour, minutes)
+        self._minutes = (minutes + 60 * hour) % (60*24)
 
     def __repr__(self) -> str:
-        return "{}:{}".format(str(self._hour).zfill(2), str(self._minutes).zfill(2))
+        return "{}:{}".format(str(self._minutes // 60).zfill(2), str(self._minutes % 60).zfill(2))
 
     def __eq__(self, other: 'Clock') -> bool:
-        return str(self) == str(other)
+        return repr(self) == repr(other)
 
     def __add__(self, minutes: int) -> 'Clock':
-        self._hour, self._minutes = self.convert_to_clock(0, self.convert_to_minutes() + minutes)
+        self._minutes = (self._minutes + minutes) % (60*24)
         return self
 
     def __sub__(self, minutes: int) -> 'Clock':
-        self._hour, self._minutes = self.convert_to_clock(0, self.convert_to_minutes() - minutes)
+        self._minutes = (self._minutes - minutes) % (60*24)
         return self
-
-    def convert_to_minutes(self) -> int:
-        return self._hour * 60 + self._minutes
-
-    def convert_to_clock(self, hours: int, minutes: int) -> (int, int):
-        hour = ((hours % 24) + ((minutes // 60) % 24)) % 24
-        minute = minutes % 60
-        return (hour, minute)
