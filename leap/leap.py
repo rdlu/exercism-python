76cac4cc16a2666c9e44ef3965f408caae2c5736
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-26 11:18:05 -0300

    leap year

---
+++ leap/leap.py
@@ -0,0 +1,8 @@
+def leap_year(year: int) -> bool:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+        else:
+            return True
+    return False
