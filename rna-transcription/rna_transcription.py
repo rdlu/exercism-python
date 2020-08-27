e4b23f06b0f48491a85b44d7503c6d9891972fac
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-27 11:26:04 -0300

    rna transcription, using translate

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,9 +1,2 @@
-rna_mapping = {
-    'G': 'C',
-    'C': 'G',
-    'T': 'A',
-    'A': 'U'
-}
-
 def to_rna(dna_strand: str) -> str:
-    return ''.join([rna_mapping[dna_unit] for dna_unit in dna_strand])
+    return dna_strand.translate(str.maketrans('GCTA', 'CGAU'))
