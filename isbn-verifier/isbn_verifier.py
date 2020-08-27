6d9556968cb3d56cc04219bb82685be9fb353a6c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-27 17:20:53 -0300

    comments

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,4 +1,7 @@
 def is_valid(isbn: str) -> bool:
+    # filter nums, convert to int
     l = [int(x) for x in filter(str.isnumeric, isbn)]
+    # handle the edge case: X
     if isbn and isbn[-1] == 'X': l.append(10)
+    # check len and sum the product of digits x position
     return len(l) == 10 and sum([l[10-i] * i for i in range(1,11)]) % 11 == 0
