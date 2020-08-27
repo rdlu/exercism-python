4e66f57c06bb3236e9fdf5a95c93e4fc8bdb997b
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-27 10:48:48 -0300

    pangram: alternative with set

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 def is_pangram(sentence: str) -> bool:
     filtered_sentence = filter(str.isalpha, sentence.lower())
-    return len(Counter(filtered_sentence).keys()) == 26
+    return len(set(filtered_sentence)) == 26
