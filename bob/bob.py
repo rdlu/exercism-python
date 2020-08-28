392410389ae73982a5264e089da054e82604289a
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-28 09:53:58 -0300

    bob

---
+++ bob/bob.py
@@ -0,0 +1,20 @@
+def response(hey_bob:str) -> str:
+    q = hey_bob.strip()
+    if is_question(q) and is_yell(q):
+        return "Calm down, I know what I'm doing!"
+    if is_question(q):
+        return "Sure."
+    if is_yell(q):
+        return "Whoa, chill out!"
+    if is_silence(q):
+        return "Fine. Be that way!"
+    return "Whatever."
+
+def is_question(q:str) -> bool:
+    return q and q[-1] == '?'
+
+def is_yell(q:str) -> bool:
+    return q.upper() == q and q.lower() != q
+
+def is_silence(q:str) -> bool:
+    return q == ''
