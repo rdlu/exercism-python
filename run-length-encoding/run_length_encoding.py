564d9eb4520c2b29af7ffc9327381dc5816ed497
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-31 14:34:07 -0300

    RLE using Regex

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,25 +1,14 @@
-from itertools import groupby
+import re
 
 def decode(data: str) -> str:
-    decoded = ''
-    count = ''
-    for char, _group in groupby(data):
-        if char.isnumeric():
-            count += str(char)
-        elif not count:
-            decoded += str(char)
-        else:
-            decoded += str(int(count) * char)
-            count = ''
-    return decoded
+    # uses the length to "multiply" the char
+    decompress = lambda match: str(int(match.groups(0)[0]) * match.groups(0)[1])
+    # matches a digit-non_digit
+    return re.sub(r"(\d+)([^0-9])", decompress, data)
 
 def encode(data: str) -> str:
-    encoded = ''
-
-    for char, group in groupby(data):
-        section_qty = sum(1 for _ in group)
-        if section_qty > 1:
-            encoded += f"{section_qty}{char}"
-        else:
-            encoded += char
-    return encoded
+    # uses the position delta to determine the size
+    compress = lambda match: f"{match.end() - match.start()}{match.groups(0)[0]}"
+    # \1+ matches the same text as most recently matched THAT has more than 1
+    # otherwise stays untouched
+    return re.sub(r"(.)\1+", compress, data)
