41e763c6ae49c642607af4b4b0ab31661370590f
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-03 19:06:35 -0300

    better comments

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -15,10 +15,10 @@ class Allergies:
         """Compares an candidate item for allergy check against the bitmask score
         
         How it works:
-            when you receive a score decimal 10 you have binary 110
-            10 bitwise-and 110 == 2 -> True - for shellfish
-            100 bitwise-and 110 == 8 -> True - for strawberries
-            1000 bitwise-and 110 == 0 -> False for tomatoes"""
+            when you receive a score decimal 10 you have binary 1010
+            10 bitwise-and 1010 == 2 -> True - for peanuts decimal[2**1]
+            100 bitwise-and 1010 == 0 -> False - for shellfish is decimal[2**2]
+            1000 bitwise-and 1010 == 8 -> True - for strawberries decimal[2**3]"""
         return bool(self.score & 2**self.allergens.index(item))
 
     @property
