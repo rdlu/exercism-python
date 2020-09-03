9d005a191348471a5eccecec3cd6d3648f528324
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-03 10:46:33 -0300

    rotate: typing

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -3,13 +3,13 @@ def rotate(text: str, key: int) -> str:
     rotated = rotate_left(plain, key)
     return text.translate(str.maketrans(plain+plain.upper(), rotated+rotated.upper()))
 
-def rotate_left(l: list, n: int):
+def rotate_left(l: list, n: int) -> list:
     if not l: return l
     rotations = n % len(l)
     return l[rotations:] + l[:rotations]
 
 # not used, just to keep in mind
-def rotate_right(l: list, n: int):
+def rotate_right(l: list, n: int) -> list:
     if not l: return l
     ### Optimization ###
     # if n > len(l), rotate only necessary with
