80eb00d3a01d3899fa89085d3cf6aad29a9aad3a
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-03 11:42:38 -0300

    difference of squares: the omg moment

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,13 +1,9 @@
-# not used
-def square_of_sum(n: int) -> int:
-    return sum(range(1, n+1)) ** 2
-
-# not used
-def sum_of_squares(n: int) -> int:
-    return sum(i**2 for i in range(1, n+1))
-
 # research: https://iq.opengenus.org/difference-between-square-of-sum-and-sum-of-squares/
+def square_of_sum(n: int) -> int:
+    return (n * (n+1) // 2) ** 2
+
+def sum_of_squares(n: int) -> int:
+    return n * (n+1) * (2*n+1) // 6
+
 def difference_of_squares(n: int) -> int:
-    sum_1 = n * (n+1) // 2
-    sum_2 = n * (n+1) * (2*n+1) // 6
-    return sum_1 * sum_1 - sum_2
+    return square_of_sum(n) - sum_of_squares(n)
