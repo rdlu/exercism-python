c1fec0a59c1643f01322885e592a0a73dd2cc6f2
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-03 15:00:03 -0300

    anagram: docstrings

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,3 +1,25 @@
+"""Anagram utilities"""
+
 def find_anagrams(word: str, candidates: list) -> list:
-    return [candidate for candidate in candidates
-        if sorted(candidate.lower()) == sorted(word.lower()) and candidate.lower() != word.lower()]
+    """Detect anagrams on a list against a reference word
+    
+    Args:
+        word: the reference word
+        candidates: the list of words to be compared
+
+    Returns:
+        A new list with the anagrams found.
+    """
+    low_word = sorted(word.lower())
+    return [candidate for candidate in candidates if is_anagram(low_word, candidate)]
+
+def is_anagram(low_sort_word: str, candidate: str) -> bool:
+    """Determine whether two words are anagrams of each other.
+
+    Args:
+        low_sort_words: the original word, sorted and lowered.
+        candidate: the word to be compared
+
+    Returns:
+        A boolean True if the two words have the same letters in different order."""
+    return sorted(candidate.lower()) == low_sort_word and candidate.lower() != low_sort_word
