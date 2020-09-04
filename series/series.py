ee986e9363eb3fe895d0ccc08ba26cc36512104d
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-04 10:54:49 -0300

    better formatting, docs

--- series/series.py
+++ series/series.py
@@ -1,3 +1,11 @@
+"""slices 
+
+A.k.a - Grouped Slices -
+
+Returns:
+    A list of grouped slices of n length from a string
+"""
 def slices(series: str, length: int) -> list:
-    if length not in range(1, len(series)+1): raise ValueError('Value not in range')
+    if length not in range(1, len(series)+1): 
+        raise ValueError(f'Length {length} not in range for this series')
     return [series[tailcut-length:tailcut] for tailcut in range(length, len(series)+1)]
