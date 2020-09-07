55054e617c16a21ab80ff052125fd677132f646c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-07 16:43:24 -0300

    comments

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,15 @@
 from typing import List
 
-def sum_of_multiples(limit: int, multiples: List[int]) -> int:
+def sum_of_multiples(limit: int, factors: List[int]) -> int:
+    """Sum of Multiples
+
+	Args:
+        limit: the limit
+	    factors: a list of numbers to find multiples of
+
+	Returns: 
+        the sum of all multiples of the factors up to, but not including the limit
+	"""
     # set comprehension (not a list, to avoid repeated nums)
-    return sum({ i for m in multiples if m != 0
+    return sum({ i for m in factors if m != 0
         for i in range(m, limit, m) })
