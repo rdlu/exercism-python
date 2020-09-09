98a0a60fa8e742c3b887073a794270e33f2e972e
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-09 17:09:02 -0300

    flatten naive for, passing tests

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -2,10 +2,9 @@ from typing import List
 
 def flatten(iterable: List) -> List:
     flat = []
-    while iterable:
-        el = iterable.pop()
-        if type(el) == list:
-            iterable.extend(el)
-        else:
+    for el in iterable:
+        if isinstance(el, list):
+            flat.extend(flatten(el))
+        elif el is not None:
             flat.append(el)
-    return sorted(flat)
+    return flat
