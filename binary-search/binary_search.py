3d56d369b69b2c370b44e303ecedf1a93c4162d5
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-09 18:22:11 -0300

    binary search, all tests ok

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -2,8 +2,8 @@ from typing import List
 
 def find(search_list: List, value: int) -> int:
     l = 0
-    r = len(search_list)
-    while l <= r and r > 0:
+    r = len(search_list) - 1
+    while l <= r:
         m = (l + r) // 2
         if search_list[m] < value:
             l = m + 1
@@ -11,4 +11,4 @@ def find(search_list: List, value: int) -> int:
             r = m - 1
         else:
             return m
-    raise ValueError('not found')
+    raise ValueError(f"{value} not found in {search_list}")
