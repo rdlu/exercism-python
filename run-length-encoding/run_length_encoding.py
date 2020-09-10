0c91a31e9ade5ecbeca82beba826c813598a99d1
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-10 11:53:24 -0300

    RLE after flake8

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -2,16 +2,16 @@ import re
 
 
 def decode(data: str) -> str:
-    # uses the length to "multiply" the char
     def decompress(match):
+        # uses the length to "multiply" the char
         return str(int(match.groups(0)[0]) * match.groups(0)[1])
     # matches a digit-non_digit
     return re.sub(r"(\d+)([^0-9])", decompress, data)
 
 
 def encode(data: str) -> str:
-    # uses the position delta to determine the size
     def compress(match):
+        # uses the position delta to determine the size
         return f"{match.end() - match.start()}{match.groups(0)[0]}"
     # \1+ matches the same text as most recently matched THAT has more than 1
     # otherwise stays untouched
