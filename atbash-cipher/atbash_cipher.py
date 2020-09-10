988a08feffec954a0206fa957cc4c6426179fc78
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-10 10:34:42 -0300

    atbash docs

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,17 +1,22 @@
 from string import ascii_lowercase, punctuation, whitespace
 
 reverse_ci = ascii_lowercase[::-1]
-trans_engine = str.maketrans(ascii_lowercase, reverse_ci, punctuation+whitespace)
+trans_engine = str.maketrans(ascii_lowercase, reverse_ci, punctuation + whitespace)
+
 
 def encode(plain_text: str) -> str:
+    """Encodes a text using Atbash Cipher"""
     return _chunk_text(_translate(plain_text))
 
 
 def decode(ciphered_text: str) -> str:
+    """Decodes a text using Atbash Cipher"""
     return _translate(ciphered_text)
+
 
 def _translate(text: str) -> str:
     return text.lower().translate(trans_engine)
+
 
 def _chunk_text(text: str, size: int = 5) -> str:
     if len(text) <= size:
