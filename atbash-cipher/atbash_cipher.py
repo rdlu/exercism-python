b3e8892938d1ee776c2d4bb38acd940f34e73c19
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-10 10:01:13 -0300

    atbash second ver, all tests ok

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,13 +1,19 @@
-plain_ci = 'abcdefghijklmnopqrstuvwxyz'
-reverse_ci = ''.join(reversed(plain_ci))
+from string import ascii_lowercase, punctuation, whitespace
+
+reverse_ci = ascii_lowercase[::-1]
+trans_engine = str.maketrans(ascii_lowercase, reverse_ci, punctuation+whitespace)
 
 def encode(plain_text: str) -> str:
-    clean_plain = plain_text.lower().replace(" ", "")
-    translated = clean_plain.translate(str.maketrans(plain_ci, reverse_ci))
-    return translated
+    return _chunk_text(_translate(plain_text))
 
 
 def decode(ciphered_text: str) -> str:
-    clean_ciphered = ciphered_text.lower().replace(" ", "")
-    translated = clean_ciphered.translate(str.maketrans(reverse_ci, plain_ci))
-    return translated
+    return _translate(ciphered_text)
+
+def _translate(text: str) -> str:
+    return text.lower().translate(trans_engine)
+
+def _chunk_text(text: str, size: int = 5) -> str:
+    if len(text) <= size:
+        return text
+    return text[:size] + " " + _chunk_text(text[size:])
