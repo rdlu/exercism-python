45dcf19937c611343e106991a8c2f1c97b24a172
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-15 11:22:05 -0300

    phone number with re

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,11 +1,18 @@
+import re
+
+
 class PhoneNumber:
     def __init__(self, number: str):
-        self.number = ''.join(list(filter(str.isdigit, number)))
-        self.validate()
+        self.number = self.__clean_number(number)
+        self.__validate()
 
-    def validate(self):
-        if len(self.number) == 11 and self.number[0] == '1':
-            self.number = self.number[1:]
+    def __clean_number(self, digits: str) -> str:
+        num = re.sub(r'[^\w]', r'', digits)
+        if len(num) == 11 and num[0] == '1':
+            num = num[1:]
+        return num
+
+    def __validate(self):
         if len(self.number) != 10:
             raise ValueError('Phone number is invalid')
         if self.number[0] == '1' or self.number[0] == '0':
@@ -17,5 +24,9 @@ class PhoneNumber:
     def area_code(self):
         return self.number[0:3]
 
+    @property
+    def exchange_code(self):
+        return self.number[3:6]
+
     def pretty(self):
-        return f"({self.area_code})-{self.number[3:6]}-{self.number[6:]}"
+        return f"({self.area_code})-{self.exchange_code}-{self.number[6:]}"
