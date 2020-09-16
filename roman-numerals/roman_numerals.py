330314bcf77235bddd76a3fd4f397079f997ceb2
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-16 17:04:24 -0300

    roman numerals using dict

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,15 +1,27 @@
-# found original using C# at http://www.blackwasp.co.uk/NumberToRoman.aspx
+numerals_map = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+
+
 def roman(number: int) -> str:
     """Converts a integer Number to a string Roman Numeral"""
     if number < 0 or number > 3999:
         raise ValueError("Value must be in the range 0 - 3999")
-    if number == 0:
-        return "N"
-    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-    result = []
-    for i in range(13):
-        while number >= values[i]:
-            number -= values[i]
-            result.append(numerals[i])
-    return ''.join(result)
+    result = ''
+    for value, numeral in numerals_map.items():
+        while number >= value:
+            number -= value
+            result += numeral
+    return result
