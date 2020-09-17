8dc15e04fcc75fb332c0cd62fb240e942b451a0d
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-17 15:46:11 -0300

    prime factors

---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def factors(value: int) -> List[int]:
+    factors = []
+    divisor = 2
+    current_value = value
+    while current_value > 1:
+        remainder, quotient = divmod(current_value, divisor)
+        if quotient == 0:
+            factors.append(divisor) 
+            current_value = remainder
+        else:
+            divisor += 1
+    return factors
