67ff27e28d9153163f75440e8968000b1891c44d
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-18 16:56:43 -0300

    grains using bitshift and comments

--- grains/grains.py
+++ grains/grains.py
@@ -2,8 +2,10 @@ def square(number: int) -> int:
     # not in range(1, 65) is a possibility
     if not 1 <= number <= 64:
         raise ValueError(f'Number {number} outside of the valid range (1-64)')
-    return 2 ** (number - 1)
+    # return 2 ** (number - 1) using bitshift
+    return 1 << number - 1
 
 
 def total() -> int:
+    # it faster to just to hardcode  the 2 ** 64 - 1 number
     return sum(square(i) for i in range(1, 65))
