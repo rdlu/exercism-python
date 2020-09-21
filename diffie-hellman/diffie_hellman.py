0d3fcc52f22f41a53882f3b266011a8ecb8fe78f
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-21 16:15:12 -0300

    diffie hellman types

--- diffie-hellman/diffie_hellman.py
+++ diffie-hellman/diffie_hellman.py
@@ -1,15 +1,15 @@
 from secrets import choice
 
 
-def private_key(p):
+def private_key(p: int) -> int:
     return choice(range(2, p))
 
 
-def public_key(p, g, our_private):
+def public_key(p: int, g: int, our_private: int) -> int:
     # A = g**a mod p
     return pow(g, our_private, p)
 
 
-def secret(p, their_public, our_private):
+def secret(p: int, their_public: int, our_private: int) -> int:
     # s = B**a mod p
     return pow(their_public, our_private, p)
