c9cf15e9920571e4afc05fd5fc6be4d74b50c07b
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 10:02:27 -0300

    coin change doc

--- change/change.py
+++ change/change.py
@@ -7,11 +7,11 @@ def find_fewest_coins(coins: list, target: int) -> list:
     if target == 0:
         return []
     if target < min(coins):
-        raise ValueError('Cannot do this. Wrong target.')
+        raise ValueError(f'Can\'t give change for #{target}')
 
     for i in range(1, target // min(coins) + 1):
         for candidate_list in generate_candidate_list(sorted(coins), i):
             if sum(candidate_list) == target:
                 return list(candidate_list)
 
-    raise ValueError('No change possible.')
+    raise ValueError('No change possible')
