42f308322edff8827b853890294d1dd496b03f7d
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 09:57:26 -0300

    coin change boilerplate

--- series/series.py
+++ series/series.py
@@ -1,15 +1,14 @@
 from typing import List
 
 
-def slices(series: str, length: int) -> List[List[str]]:
+def slices(s: str, size: int) -> List[List[str]]:
     """Return all substrings of `size`, given a string `s`.
     If `size` is greater than the length of `s`, or less than 1, return an empty list
 
     :param series: str:
     :param length: int:
     :returns: A list of grouped slices of n length from a string
-
     """
-    if length < 1:
+    if size < 1:
         return []
-    return [series[tailcut - length:tailcut] for tailcut in range(length, len(series) + 1)]
+    return [s[tailcut - size:tailcut] for tailcut in range(size, len(size) + 1)]
