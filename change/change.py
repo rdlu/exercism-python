0da46387040d768619cd500c0e03b9249ca7e29c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 09:57:45 -0300

    coin change solution

--- change/change.py
+++ change/change.py
@@ -1,2 +1,17 @@
-def find_fewest_coins(coins, target):
-    pass
+# combinations_with_replacement is greedy and order-stable, will use to generate candidates
+# https://docs.python.org/3.8/library/itertools.html#itertools.combinations_with_replacement
+from itertools import combinations_with_replacement as generate_candidate_list
+
+
+def find_fewest_coins(coins: list, target: int) -> list:
+    if target == 0:
+        return []
+    if target < min(coins):
+        raise ValueError('Cannot do this. Wrong target.')
+
+    for i in range(1, target // min(coins) + 1):
+        for candidate_list in generate_candidate_list(sorted(coins), i):
+            if sum(candidate_list) == target:
+                return list(candidate_list)
+
+    raise ValueError('No change possible.')
