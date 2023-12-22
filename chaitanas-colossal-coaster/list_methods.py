eca45a5395cdfdc59740a311719da7084d3110a0
Rodrigo Dlugokenski <git@dlu.one>
2023-12-22 09:15:29 -0300

    lint the easy peasy

--- chaitanas-colossal-coaster/list_methods.py
+++ chaitanas-colossal-coaster/list_methods.py
@@ -13,9 +13,9 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     if ticket_type == 1:
         express_queue.append(person_name)
         return express_queue
-    else:
-        normal_queue.append(person_name)
-        return normal_queue
+
+    normal_queue.append(person_name)
+    return normal_queue
 
 
 def find_my_friend(queue, friend_name):
