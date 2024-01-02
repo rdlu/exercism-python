b4dd89372b7fd5d3d40df22b5e14f9dc03efb5b4
Rodrigo Dlugokenski <git@dlu.one>
2024-01-02 17:15:27 -0300

    tuples using guard instead else

--- tisbury-treasure-hunt/tuples.py
+++ tisbury-treasure-hunt/tuples.py
@@ -44,8 +44,8 @@ def create_record(azara_record, rui_record):
 
     if compare_records(azara_record, rui_record):
         return azara_record + rui_record
-    else:
-        return "not a match"
+
+    return "not a match"
 
 
 def clean_up(combined_record_group):
