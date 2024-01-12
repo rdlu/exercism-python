74c24f494cb9034dca6b4f46668b3d47daae4fb6
Rodrigo Dlugokenski <git@dlu.one>
2024-01-12 08:06:09 -0300

    darts branchless

--- darts/darts.py
+++ darts/darts.py
@@ -1,12 +1,5 @@
 def score(x, y):
+    """Branchless version (avoiding if-then-else) of a scoring function for a darts game."""
     radius = (x**2 + y**2) ** 0.5
-    if radius <= 1:
-        return 10
 
-    if radius <= 5:
-        return 5
-
-    if radius <= 10:
-        return 1
-
-    return 0
+    return 10 * (radius <= 1) + 5 * (radius <= 5) + 1 * (radius <= 10)
